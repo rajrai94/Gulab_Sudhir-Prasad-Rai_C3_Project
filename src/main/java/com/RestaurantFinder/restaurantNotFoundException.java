b6625e8859e8cb278ba54373be7878450a3843ee
rajrai94 <62497040+rajrai94@users.noreply.github.com>
2021-08-05 01:05:33 +0800

    Delete src directory

--- src/main/java/com/RestaurantFinder/restaurantNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.RestaurantFinder;
-
-public class restaurantNotFoundException extends Throwable {
-    public restaurantNotFoundException(String restaurantName) {
-        super(restaurantName);
-    }
-}
